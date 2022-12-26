e3d54976c738130c6a3aee0f581a4fd22e8ad055
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2022-12-26 18:17:08 +0530

    Kth maximum and minimum element

---
+++ KthMaxAndMin.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class KthMaxAndMin {
+    public static void main(String[] args) {
+        int K=2;
+        int[] A={1,4,3,2,6,5,6,7,8};
+        int n=A.length;
+        Arrays.sort(A);
+System.out.println(A[K-1] + " is kth minimum");
+System.out.println(A[n-K] +" is Kth maximum");
+    }
+}
