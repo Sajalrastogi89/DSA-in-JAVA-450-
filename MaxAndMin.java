0152136b318cbbd530b7de46e90a368144078586
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2022-12-26 18:05:29 +0530

    Max added

---
+++ MaxAndMin.java
@@ -0,0 +1,12 @@
+public class MaxAndMin{
+    public static void main(String[] args) {
+        int[] A={1,2,3,1,2,5,6,3,5,2,8,7,5,2};
+        int Max=A[0];
+        int Min=A[0];
+        for(int i=1;i<A.length;i++){
+           Max= Math.max(Max,A[i]);
+           Min= Math.min(Min,A[i]);}
+        System.out.println("Max:"+Max);
+        System.out.println("Min:"+Min);
+    }
+}
